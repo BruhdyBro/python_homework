dfc55314c7028edd096fe846b9fd50264d1dda72
BruhdyBro <broudynegron@gmail.com>
2026-08-04 18:29:54 -0400

    Completed Assignment 4

---
+++ assignment4/assignment4.py
@@ -0,0 +1,118 @@
+import pandas as pd
+import numpy as np
+
+
+# Task 1
+data = {
+    "Name": ["Alice", "Bob", "Charlie"],
+    "Age": [25, 30, 35],
+    "City": ["New York", "Los Angeles", "Chicago"]
+}
+
+print("-=-=-=-Original Dataframe-=-=-=-")
+task1_data_frame = pd.DataFrame(data)
+print(task1_data_frame)
+print("\n")
+
+
+print("-=-=-=-With Salary-=-=-=-")
+task1_with_salary = task1_data_frame.copy()
+task1_with_salary['Salary'] = [70000, 80000, 90000]
+print(task1_with_salary)
+print("\n")
+
+
+print("-=-=-=-Age + 1-=-=-=-")
+task1_older = task1_with_salary.copy()
+task1_older['Age'] = task1_older['Age'].apply(lambda x: x+1)
+print(task1_older)
+print("\n")
+
+task1_older.to_csv("employees.csv",index=False)
+
+
+
+
+
+# Task 2
+print("-=-=-=-Read from CSV-=-=-=-")
+task2_employees = pd.read_csv("employees.csv", index_col=False)
+print(task2_employees)
+print("\n")
+
+print("-=-=-=-JSON Employees-=-=-=-")
+json_employees = pd.read_json("additional_employees.json")
+print(json_employees)
+print("\n")
+
+print("-=-=-=-Dataframe Concat-=-=-=-")
+more_employees = pd.concat([task2_employees,json_employees], ignore_index=True)
+print(more_employees)
+print("\n")
+
+
+
+
+
+# Task 3
+print("-=-=-=-Head Assignment-=-=-=-")
+first_three = more_employees.head(3)
+print(first_three)
+print("\n")
+
+print("-=-=-=-Tail Assignment-=-=-=-")
+last_two = more_employees.tail(2)
+print(last_two)
+print("\n")
+
+print("-=-=-=-Shape Assignment-=-=-=-")
+employee_shape = more_employees.shape
+print(employee_shape)
+print("\n")
+
+print("-=-=-=-Info Assignment-=-=-=-")
+print(more_employees.info())
+print("\n")
+
+
+
+
+
+# Task 4
+print("-=-=-=-Dirty Data Read-=-=-=-")
+dirty_data = pd.read_csv("dirty_data.csv", index_col=False)
+print(dirty_data)
+print("\n")
+
+print("-=-=-=-Data Cleaning Operations-=-=-=-")
+clean_data = dirty_data.copy()
+print(clean_data)
+print("\n")
+
+print("-=-=-=-Dropping Duplicates-=-=-=-")
+clean_data = clean_data.drop_duplicates()
+print(clean_data)
+print("\n")
+
+print("-=-=-=-Fixing Empty and Non-Numeric Age-=-=-=-")
+clean_data['Age'] = pd.to_numeric(clean_data['Age'], errors="coerce")
+clean_data['Age'] = clean_data['Age'].fillna(clean_data['Age'].mean())
+print(clean_data)
+print("\n")
+
+print("-=-=-=-Fixing Empty and Non-Numeric Salary-=-=-=-")
+clean_data['Salary'] = pd.to_numeric(clean_data['Salary'], errors="coerce")
+clean_data["Salary"] = clean_data["Salary"].fillna(clean_data['Salary'].median())
+print(clean_data)
+print("\n")
+
+print("-=-=-=-Fixing Hire Date-=-=-=-")
+clean_data["Hire Date"] = pd.to_datetime(clean_data['Hire Date'], format='mixed', errors='coerce')
+print(clean_data)
+print("\n")
+
+print("-=-=-=-Fixing Name and Department-=-=-=-")
+clean_data['Name'] = clean_data['Name'].str.upper()
+clean_data['Department'] = clean_data['Department'].str.upper()
+print(clean_data)
+print("\n")
